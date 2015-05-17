1fdff424c33d96ac236f392f8f80a694e78cda35
Elena Caballero <elena.caballero@unitec.edu>
2015-05-17 11:51:59 -0600

    corrigiendo main

--- fscursorlist.cpp
+++ fscursorlist.cpp
@@ -139,10 +139,8 @@ int FSCursorList::getCapacity()const{
 int FSCursorList::avail()const{
 	for (int i = 0; i < size; ++i)
 	{
-		if (rows[i].data == NULL)
-		{
+		if (rows[i].data == NULL )
 			return i;
-		}
 	}
 	return -1;
 }
